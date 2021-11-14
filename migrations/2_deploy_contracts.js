8e3b601da121f03933292b3f454fe62f904155c5
Max Korolev <nrjshka@gmail.com>
2021-11-14 23:11:57 +0100

    Added: kyc and test support

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,16 @@
+require('dotenv').config({ path: '../.env' });
+
 var MyToken = artifacts.require("./MyToken.sol");
 var MyTokenSale = artifacts.require("./MyTokenSale.sol");
+var KycContract = artifacts.require("./KycContract.sol");
 
-module.exports = function(deployer) {
+module.exports = async deployer => {
   let addr = await web3.eth.getAccounts();
 
-  await deployer.deploy(MyToken, 1000000000);
-  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+  await deployer.deploy(MyToken, Number(process.env.INITIAL_TOKENS));
+  await deployer.deploy(KycContract);
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address, KycContract.address);
 
   let tokenInstance = await MyToken.deployed();
-  await tokenInstance.transfer(MyTokenSales.address, 1000000000);
+  await tokenInstance.transfer(MyTokenSale.address, Number(process.env.INITIAL_TOKENS));
 };
--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,23 +1,22 @@
-const Token = artifacts.require("MyToken");
+require('dotenv').config({ path: '../.env' });
 
-const { expect } = require("chai");
-var chai = require("chai");
+const Token = artifacts.require("MyToken.sol");
 
+const chai = require("./chaisetup.js");
 const BN = web3.utils.BN;
+const expect = chai.expect;
 
-const chainBN = require("chai-bn")(BN);
-chai.use(chainBN);
-
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-
-contract("Token Test", async accounts => {
+contract("Token Test", accounts => {
   const [initialHolder, recipient, anotherAccount] = accounts;
 
+  beforeEach(async () => {
+    this.myToken = await Token.new(process.env.INITIAL_TOKENS);
+  });
+
   it("All tokens should be on my account", async () => {
-    let instance = await Token.deployed();
+    let instance = this.myToken;
     let totalSupply = await instance.totalSupply();
 
-    await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+    return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
   });
 });
